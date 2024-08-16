e71609caa4bb2a279969d81dca27339e465058a4
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-08-16 18:44:00 +0600

    added search box

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,6 +20,24 @@ const Home = () => {
 
   return (
     <div className="my-10 max-w-[95%] mx-auto">
+      {/* search box */}
+      <div className="max-w-[25%] mx-auto my-4">
+        <label className="input input-bordered flex items-center gap-2">
+          <input type="text" className="grow" placeholder="Search" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </div>
       {/* show products */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
         {products.map((product, idx) => (
