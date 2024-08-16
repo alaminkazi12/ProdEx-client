529efbe42aa36f3745452134409616c88491c4db
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-08-16 17:48:14 +0600

    added product card

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,68 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ProductCard from "./components/ProductCard";
+import {
+  TbPlayerTrackPrevFilled,
+  TbPlayerTrackNextFilled,
+} from "react-icons/tb";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/products").then((res) => {
+      setProducts(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1 className="mx-10 my-4">Home</h1>
+    <div className="my-10 max-w-[95%] mx-auto">
+      {/* show products */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        {products.map((product, idx) => (
+          <ProductCard key={idx} product={product}></ProductCard>
+        ))}
+      </div>
+      {/* pagination */}
+
+      <div className="flex items-center justify-center mt-10">
+        <div className="join">
+          <button className="join-item btn btn-square">
+            <TbPlayerTrackPrevFilled />
+          </button>
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="1"
+            defaultChecked
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="2"
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="3"
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="4"
+          />
+          <button className="join-item btn btn-square">
+            <TbPlayerTrackNextFilled />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ProductCard = ({ product }) => {
+  console.log(product);
+  return (
+    <div>
+      <div className="card bg-base-100 shadow-xl h-96">
+        {" "}
+        {/* Set a fixed height */}
+        <figure className="flex-1">
+          {" "}
+          {/* Flex-1 to make image take up remaining space */}
+          <img src={product.product_image} alt={product.product_name} />
+        </figure>
+        <div className="card-body flex-1">
+          {" "}
+          {/* Flex-1 to make content take up remaining space */}
+          <h2 className="card-title">{product.product_name}</h2>
+          <p>{product.description}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">View Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
